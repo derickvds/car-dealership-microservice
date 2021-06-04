fbdc799ef41bc90e69aa938a50a656a235477126
derickvds <derickvds@gmail.com>
2021-06-04 22:22:59 +0200

    Added cars api with basic crud and db seed

---
+++ CarDealershipMicroservice/Cars.API/Data/CarsAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cars.API.Models;
+
+namespace Cars.API.Data
+{
+    public class CarsAPIContext : DbContext
+    {
+        public CarsAPIContext (DbContextOptions<CarsAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cars.API.Models.Car> Car { get; set; }
+    }
+}
---
+++ CarDealershipMicroservice/Cars.API/Data/CarsContextSeed.cs
@@ -0,0 +1,87 @@
+﻿using Cars.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars.API.Data
+{
+    public class CarsContextSeed
+    {
+        public static void SeedAsync(CarsAPIContext carsContext)
+        {
+            if (!carsContext.Car.Any())
+            {
+                var movies = new List<Car>
+                {
+                    new Car
+                    {
+                        Id = 1,
+                        Model = "2",
+                        Brand = "Mazda",
+                        DateAcquired = new DateTime(2021, 4, 22),
+                        PreviousOwners = 1
+                    },
+                    new Car
+                    {
+                        Id = 2,
+                        Model = "2 Series",
+                        Brand = "BMW",
+                        DateAcquired = new DateTime(2021, 1, 11),
+                        PreviousOwners = 2
+                    },
+                    new Car
+                    {
+                        Id = 3,
+                        Model = "MX-5",
+                        Brand = "Mazda",
+                        DateAcquired = new DateTime(2020, 9, 5),
+                        PreviousOwners = 1
+                    },
+                    new Car
+                    {
+                        Id = 4,
+                        Model = "Hilux",
+                        Brand = "Toyota",
+                        DateAcquired = new DateTime(2019, 10, 5),
+                        PreviousOwners = 2
+                    },
+                    new Car
+                    {
+                        Id = 5,
+                        Model = "Golf",
+                        Brand = "Volkswagen",
+                        DateAcquired = new DateTime(2020, 6, 7),
+                        PreviousOwners = 1
+                    },
+                    new Car
+                    {
+                        Id = 6,
+                        Model = "Polo",
+                        Brand = "Volkswagen",
+                        DateAcquired = new DateTime(2021, 5, 5),
+                        PreviousOwners = 2
+                    },
+                    new Car
+                    {
+                        Id = 7,
+                        Model = "Ranger",
+                        Brand = "Ford",
+                        DateAcquired = new DateTime(2015, 2, 5),
+                        PreviousOwners = 4
+                    },
+                    new Car
+                    {
+                        Id = 8,
+                        Model = "3",
+                        Brand = "Mazda",
+                        DateAcquired = new DateTime(2021, 5, 5),
+                        PreviousOwners = 1
+                    }
+                };
+                carsContext.Car.AddRange(movies);
+                carsContext.SaveChanges();
+            }
+        }
+    }
+}
